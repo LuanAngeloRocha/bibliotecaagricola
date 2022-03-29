cd874c85a4cae3f5a84d729d61a1aa6b92b40f92
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2022-03-29 18:49:04 +0000

    atualizando apllication

--- scr/main/java/application/Application.java
+++ scr/main/java/application/Application.java
@@ -1,6 +1,13 @@
-package main.java.application;
+package application;
 
+import org.springframework.boot.autoconfigure.SpringBootAplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootAplication
 public class Application {
  
-    
+    public static void main(String[] args) {
+        SpringBootAplication.run(Aplication.class,args);
+
+    }
 }
